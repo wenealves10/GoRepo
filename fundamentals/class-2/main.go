b1b3529cef6e2820cd5c413d17bac595ded82500
Wene Alves de Oliveira <weneplay5@gmail.com>
2021-10-30 21:14:29 -0300

    Generate Number Random

--- fundamentals/class-2/main.go
+++ fundamentals/class-2/main.go
@@ -3,10 +3,26 @@ package main
 import (
 	"fmt"
 	"math/rand"
+	"time"
 )
 
 func main() {
-	for i := 0; i < 100; i++ {
-		fmt.Println(rand.Intn(10-1) + 1)
+
+	x1 := rand.NewSource(time.Now().UnixNano())
+	y1 := rand.New(x1)
+
+	number := y1.Intn(10)
+	var userNumber int
+
+	fmt.Print("Digite um número: ")
+	fmt.Scanln(&userNumber)
+
+	if number == userNumber {
+		fmt.Print("Você acertou!!!\n")
+	} else if number < userNumber {
+		fmt.Printf("Você chutou acima!!! \nValor era: %d\n", number)
+	} else if number > userNumber {
+		fmt.Printf("Você chutou abaixo!!! \nValor era: %d\n", number)
 	}
+
 }
