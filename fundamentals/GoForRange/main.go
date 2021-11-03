171b7fda41fb6b3fc3f763962b86cdc371233997
Wene Alves de Oliveira <weneplay5@gmail.com>
2021-11-03 12:06:45 -0300

    New

--- fundamentals/GoForRange/main.go
+++ fundamentals/GoForRange/main.go
@@ -2,15 +2,13 @@ package main
 
 import (
 	"fmt"
-
-	goclean "github.com/wenealves10/goClean"
 )
 
 func main() {
 	numbers := [...]int{1, 2, 3, 4, 5, 6, 5, 8}
 
 	for i, num := range numbers {
-		goclean.Clear()
+
 		fmt.Printf("index: %d  element: %d\n", i+1, num)
 	}
 
