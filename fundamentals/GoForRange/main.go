3a791583f905a7799a1b801001f629ed0a9bd716
Wene Alves de Oliveira <weneplay5@gmail.com>
2021-11-03 11:57:00 -0300

    Created Packages

--- fundamentals/GoForRange/main.go
+++ fundamentals/GoForRange/main.go
@@ -1,11 +1,16 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	goclean "github.com/wenealves10/goClean"
+)
 
 func main() {
 	numbers := [...]int{1, 2, 3, 4, 5, 6, 5, 8}
 
 	for i, num := range numbers {
+		goclean.Clear()
 		fmt.Printf("index: %d  element: %d\n", i+1, num)
 	}
 
---
+++ fundamentals/go.mod
@@ -0,0 +1,3 @@
+module github.com/wenealves10/GoRepo/fundamentals
+
+go 1.17
